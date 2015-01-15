7c7bfb3c2f320cf00237fecd7f1159f97342ef7b
nhatbang <nhatbang@gmail.com>
2015-01-15 11:04:08 +0700

    Commit de category

--- src/main/java/com/icoding/trangphuc/domain/Category.java
+++ src/main/java/com/icoding/trangphuc/domain/Category.java
@@ -20,7 +20,7 @@ public class Category {
 
 	private CategoryStatus categoryStatus;
 	private Category parent;
-	
+
 	@OneToOne(fetch = FetchType.LAZY)
 	@PrimaryKeyJoinColumn
 	private Article article;
