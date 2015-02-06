934dc78c344056cf038c6161894b682eeb5f39e0
nhatbang <nhatbang@gmail.com>
2015-02-06 11:51:21 +0700

    --- Add Category Service ---

---
+++ src/main/java/com/icoding/trangphuc/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.icoding.trangphuc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.icoding.trangphuc.dao.CategoryDao;
+
+public class CategoryService {
+
+	@Autowired
+	private CategoryDao categoryDao;
+	
+	
+
+}
